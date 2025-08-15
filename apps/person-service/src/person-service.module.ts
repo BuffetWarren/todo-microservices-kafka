fa4458e46a24c72d7b65b534678ecc7fc55bbac9
Warren <fotiewarren50@gmail.com>
2025-08-15 20:37:49 +0100

    logic of person-service implemented

--- apps/person-service/src/person-service.module.ts
+++ apps/person-service/src/person-service.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PersonServiceController } from './person-service.controller';
 import { PersonServiceService } from './person-service.service';
+import { PersonsController } from './modules/persons/persons.controller';
+import { PersonService } from './modules/persons/persons.service';
+import { ConfigModule } from '@nestjs/config';
+import { PersonsModule } from './modules/persons/person.module';
 
 @Module({
-  imports: [],
-  controllers: [PersonServiceController],
-  providers: [PersonServiceService],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+    }),PersonsModule],
+  controllers: [PersonServiceController, PersonsController],
+  providers: [PersonServiceService, PersonService],
 })
 export class PersonServiceModule {}
